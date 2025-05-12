b18ae19dc5a24266a58526cd1bc93d1e88356cea
Itay <itayg43@gmail.com>
2025-05-12 04:04:42 +0300

    edit: validate env values

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,17 +1,13 @@
 import http from "http";
-import "dotenv/config";
 
 import app from "./app";
+import env from "./env";
 import processHandlers from "./utils/process-handlers/process-handlers";
-
-const DEFAULT_PORT = 3000;
 
 const server = http.createServer(app);
 
-const port = process.env.SERVER_PORT || DEFAULT_PORT;
-
-server.listen(port, () => {
-  console.log(`Server is running on port: ${port}`);
+server.listen(env.PORT, () => {
+  console.log(`Server is running on port: ${env.PORT}`);
 });
 
 processHandlers();
