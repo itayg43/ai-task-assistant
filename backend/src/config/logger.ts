cf8624ba560afdc73a2450abe37ee903b7901fe4
Itay <itayg43@gmail.com>
2025-06-22 15:34:34 +0300

    edit: add error serializer to the winston logger

--- backend/src/config/logger.ts
+++ backend/src/config/logger.ts
@@ -2,9 +2,37 @@ import { createLogger, format, transports } from "winston";
 
 import { LogMeta } from "../types/log-meta";
 
+const errorSerializer = format((info) => {
+  function serialize(obj: any): any {
+    if (obj instanceof Error) {
+      return {
+        ...obj,
+        name: obj.name,
+        message: obj.message,
+        stack: obj.stack,
+      };
+    } else if (obj && typeof obj === "object") {
+      for (const key of Object.keys(obj)) {
+        obj[key] = serialize(obj[key]);
+      }
+    }
+
+    return obj;
+  }
+
+  if (info) {
+    for (const key of Object.keys(info)) {
+      info[key] = serialize(info[key]);
+    }
+  }
+
+  return info;
+});
+
 const baseLogger = createLogger({
   level: "info",
   format: format.combine(
+    errorSerializer(),
     format.colorize(),
     format.timestamp(),
     format.printf(({ timestamp, level, message, tag, ...meta }) => {
