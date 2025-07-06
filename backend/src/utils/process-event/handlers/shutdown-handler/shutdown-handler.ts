668194bbd262769c54f48603cecc0a413a5eb725
Itay <itayg43@gmail.com>
2025-07-06 21:16:45 +0300

    refactor: functions order

--- backend/src/utils/process-event/handlers/shutdown-handler/shutdown-handler.ts
+++ backend/src/utils/process-event/handlers/shutdown-handler/shutdown-handler.ts
@@ -7,35 +7,47 @@ import { ExitCallback } from "@types";
 
 const logger = createLogger(TAG.SHUTDOWN_HANDLER);
 
-const checkIfShutdownAlreadyInProgress = (shutdownView: Uint8Array) => {
+export const shutdownHandler = async (
+  server: http.Server,
+  event: string,
+  errorOrReason: unknown,
+  shutdownView: Uint8Array,
+  exitCallback: ExitCallback = process.exit
+) => {
+  logger.info(`Invoked by event: ${event}`);
+
+  if (checkIfShutdownAlreadyInProgress(shutdownView)) {
+    logger.info("Shutdown already in progress, skipping.");
+
+    return;
+  }
+
+  if (errorOrReason) {
+    logger.error(`Shutting down due to ${event}:`, {
+      errorOrReason,
+    });
+  } else {
+    logger.info(`Received ${event}. Shutting down...`);
+  }
+
+  await processShutdown(server, errorOrReason, exitCallback);
+};
+
+function checkIfShutdownAlreadyInProgress(shutdownView: Uint8Array) {
   // use atomic compare-and-exchange to ensure only one handler proceeds
   const expected = SHUTDOWN_STATE.NOT_SHUTTING_DOWN;
   const replacement = SHUTDOWN_STATE.SHUTTING_DOWN;
 
-  const result =
-    Atomics.compareExchange(shutdownView, 0, expected, replacement) !==
-    expected;
+  return (
+    Atomics.compareExchange(shutdownView, 0, expected, replacement) !== expected
+  );
+}
 
-  return result;
-};
-
-const closeServer = async (server: http.Server): Promise<void> => {
-  return new Promise((resolve, reject) => {
-    server.close((closeError) => {
-      if (closeError) {
-        reject(closeError);
-      } else {
-        resolve();
-      }
-    });
-  });
-};
-
-const processShutdown = async (
+async function processShutdown(
   server: http.Server,
   errorOrReason: unknown,
   exitCallback: ExitCallback
-) => {
+) {
   logger.info("Closing HTTP server...");
 
   try {
@@ -56,32 +68,16 @@ const processShutdown = async (
 
     exitCallback(EXIT_CODE.ERROR);
   }
-};
+}
 
-export const shutdownHandler = async (
-  server: http.Server,
-  event: string,
-  errorOrReason: unknown,
-  shutdownView: Uint8Array,
-  exitCallback: ExitCallback = process.exit
-) => {
-  logger.info(`Invoked by event: ${event}`);
-
-  const isShutdownAlreadyInProgress =
-    checkIfShutdownAlreadyInProgress(shutdownView);
-  if (isShutdownAlreadyInProgress) {
-    logger.info("Shutdown already in progress, skipping.");
-
-    return;
-  }
-
-  if (errorOrReason) {
-    logger.error(`Shutting down due to ${event}:`, {
-      errorOrReason,
+async function closeServer(server: http.Server): Promise<void> {
+  return new Promise((resolve, reject) => {
+    server.close((closeError) => {
+      if (closeError) {
+        reject(closeError);
+      } else {
+        resolve();
+      }
     });
-  } else {
-    logger.info(`Received ${event}. Shutting down...`);
-  }
-
-  await processShutdown(server, errorOrReason, exitCallback);
-};
+  });
+}
