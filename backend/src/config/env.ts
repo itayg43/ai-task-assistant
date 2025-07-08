f7258c38a2bb23d0ddd5b0609544af264cd965f9
Itay <itayg43@gmail.com>
2025-07-08 17:19:53 +0300

    edit: add spaces

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -5,7 +5,9 @@ dotenv.config();
 
 export const env = cleanEnv(process.env, {
   PORT: port(),
+
   REDIS_URL: str(),
+
   REDLOCK_RETRY_COUNT: num(),
   REDLOCK_RETRY_DELAY: num(),
   REDLOCK_RETRY_JITTER: num(),
