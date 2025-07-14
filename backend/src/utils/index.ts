ff183a8a161910e194468d58770d4d935b4045dc
Itay <itayg43@gmail.com>
2025-07-14 22:10:27 +0300

    edit: default export index file

--- backend/src/utils/index.ts
+++ backend/src/utils/index.ts
@@ -1,12 +1,10 @@
 export { registerProcessEventHandlers } from "./process-event/register-process-event-handlers/register-process-event-handlers";
-export { withLock } from "./with-lock/with-lock";
 export { getElapsedTime } from "./time/time";
-export { processTokenBucket } from "./token-bucket/process-token-bucket/process-token-bucket";
 export {
   getTokenBucketKey,
   getTokenBucketLockKey,
-} from "./token-bucket/key-utils";
-export {
   getTokenBucketState,
+  processTokenBucket,
   setTokenBucketState,
-} from "./token-bucket/bucket-state-utils";
+} from "./token-bucket";
+export { withLock } from "./with-lock/with-lock";
---
+++ backend/src/utils/token-bucket/index.ts
@@ -0,0 +1,3 @@
+export { getTokenBucketState, setTokenBucketState } from "./bucket-state-utils";
+export { getTokenBucketKey, getTokenBucketLockKey } from "./key-utils";
+export { processTokenBucket } from "./process-token-bucket/process-token-bucket";
