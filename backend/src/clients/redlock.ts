3573e10bf1d8cd4bcc6c8bedcf1410a1aa83da5c
Itay <itayg43@gmail.com>
2025-07-15 05:47:26 +0300

    edit: use updated env values

--- backend/src/clients/redlock.ts
+++ backend/src/clients/redlock.ts
@@ -5,6 +5,6 @@ import { env } from "@config";
 
 export const redlock = new Redlock([redis], {
   retryCount: env.REDLOCK_RETRY_COUNT,
-  retryDelay: env.REDLOCK_RETRY_DELAY,
-  retryJitter: env.REDLOCK_RETRY_JITTER,
+  retryDelay: env.REDLOCK_RETRY_DELAY_MS,
+  retryJitter: env.REDLOCK_RETRY_JITTER_MS,
 });
