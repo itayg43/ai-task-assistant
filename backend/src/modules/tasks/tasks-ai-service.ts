07a6d65817bf2cc2092469ed381dabac36166528
Itay <itayg43@gmail.com>
2025-07-29 02:57:45 +0300

    edit: add duration of parse task to debug log

--- backend/src/modules/tasks/tasks-ai-service.ts
+++ backend/src/modules/tasks/tasks-ai-service.ts
@@ -2,22 +2,22 @@ import { openai } from "@clients/openai";
 import { createLogger } from "@config/logger";
 import { parseTaskPrompt } from "@modules/tasks/tasks-prompts";
 import { parseTaskOutputSchema } from "@modules/tasks/tasks-schemas";
+import { getCurrentTime } from "@utils/time-date";
 
 const logger = createLogger("tasksAiService");
 
 export const parseTask = async (naturalLanguage: string) => {
   const prompt = parseTaskPrompt(naturalLanguage);
 
+  const startTimestamp = getCurrentTime();
   const aiResponse = await openai.responses.create(prompt);
+  const duration = getCurrentTime() - startTimestamp;
   logger.debug("parseTask - ai response", {
     aiResponse,
+    duration: `${duration.toFixed(2)}ms`,
   });
 
   const parsedAiResponse = JSON.parse(aiResponse.output_text);
-  logger.debug("parseTask - parsed ai response", {
-    parsedAiResponse,
-  });
-
   const parsedTask = parseTaskOutputSchema.parse(parsedAiResponse);
   logger.debug("parseTask - parsed task", {
     parsedTask,
