7b3fbf1b43b6cd9f083cba0be61a03490fa370c1
Itay <itayg43@gmail.com>
2025-08-11 20:18:06 +0300

    edit: use tasks service hardcoded port

--- backend/services/ai/src/middlewares/cors.ts
+++ backend/services/ai/src/middlewares/cors.ts
@@ -1,7 +1,3 @@
 import { createCors } from "@shared/middlewares/cors/create-cors";
-import { env } from "src/env";
 
-export const cors = createCors([
-  `http://localhost:${env.SERVICE_PORT}`,
-  `http://tasks:${env.SERVICE_PORT}`,
-]);
+export const cors = createCors(["http://localhost:3001", "http://tasks:3001"]);
