215a1f64a4cf337bfda12bac373b93b99c87ebde
Itay <itayg43@gmail.com>
2025-08-11 20:02:13 +0300

    edit: use port from env

--- backend/services/ai/src/middlewares/cors.ts
+++ backend/services/ai/src/middlewares/cors.ts
@@ -1,3 +1,7 @@
 import { createCors } from "@shared/middlewares/cors/create-cors";
+import { env } from "src/env";
 
-export const cors = createCors(["http://localhost:3001", "http://tasks:3001"]);
+export const cors = createCors([
+  `http://localhost:${env.SERVICE_PORT}`,
+  `http://tasks:${env.SERVICE_PORT}`,
+]);
