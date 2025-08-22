09dab6a9b782f63487ce8167bb1e23c93bcea9e3
Itay <itayg43@gmail.com>
2025-08-22 09:42:56 +0300

    edit: validate capability input inside the controller

--- backend/services/ai/src/controllers/capabilities-controller/capabilities-controller.ts
+++ backend/services/ai/src/controllers/capabilities-controller/capabilities-controller.ts
@@ -3,13 +3,20 @@ import { StatusCodes } from "http-status-codes";
 
 import { capabilities } from "@capabilities";
 import { createLogger } from "@shared/config/create-logger";
+import { DEFAULT_RETRY_CONFIG } from "@shared/constants";
 import { withDurationAsync } from "@shared/utils/with-duration";
 import { withRetry } from "@shared/utils/with-retry";
+import { ExecuteCapabilityInput } from "@types";
 
 const logger = createLogger("capabilitiesController");
 
 export const executeCapability = async (
-  req: Request,
+  req: Request<
+    ExecuteCapabilityInput["params"],
+    unknown,
+    unknown,
+    ExecuteCapabilityInput["query"]
+  >,
   res: Response,
   next: NextFunction
 ) => {
@@ -17,41 +24,35 @@ export const executeCapability = async (
   const config = capabilities[capability as keyof typeof capabilities];
 
   try {
+    const validatedCapabilityInput = config.inputSchema.parse(req);
+
     logger.info("executeCapability - starting", {
-      capability,
-      input: {
-        body: req.body,
-        query: req.query,
-      },
+      input: validatedCapabilityInput,
     });
 
-    const { result, duration } = await withDurationAsync(
-      async () =>
-        await withRetry(
-          { maxAttempts: 3, baseDelayMs: 1000, backoffMultiplier: 2 },
-          () =>
-            config.handler({
-              body: req.body,
-              params: req.params,
-              query: req.query,
-            })
-        )
-    );
+    const {
+      result: { metadata, result },
+      duration,
+    } = await withDurationAsync(async () => {
+      return await withRetry(DEFAULT_RETRY_CONFIG, () =>
+        config.handler(validatedCapabilityInput)
+      );
+    });
 
     logger.info("executeCapability - completed", {
       capability,
+      metadata,
       result,
-      totalDuration: `${duration}ms`,
+      duration: `${duration}ms`,
     });
 
-    res.status(StatusCodes.OK).json(result);
+    res.status(StatusCodes.OK).json({
+      metadata,
+      result,
+    });
   } catch (error) {
     logger.error("executeCapability - failed", error, {
       capability,
-      input: {
-        body: req.body,
-        query: req.query,
-      },
     });
 
     next(error);
