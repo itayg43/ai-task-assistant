cc752974360f3806e33ec0864cbe25b06ad8fe26
Itay <itayg43@gmail.com>
2025-10-16 20:08:35 +0300

    edit: remove count tokens, usage of execute parse added to open ai client file

--- backend/services/ai/src/clients/openai.ts
+++ backend/services/ai/src/clients/openai.ts
@@ -1,7 +1,14 @@
 import OpenAI from "openai";
+import { ResponseCreateParamsNonStreaming } from "openai/resources/responses/responses";
 
 import { env } from "@config/env";
 
 export const openai = new OpenAI({
   apiKey: env.OPENAI_API_KEY,
 });
+
+export const executeParse = <TOutput>(
+  prompt: ResponseCreateParamsNonStreaming
+) => {
+  return openai.responses.parse<any, TOutput>(prompt);
+};
